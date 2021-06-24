9b46667f0a1b3886c27f74858a004c6de40db94c
Brian Lipsett <lipsettbrian3@gmail.com>
2021-06-24 16:35:41 -0600

    account css

--- Kotini.client/src/pages/AccountPage.vue
+++ Kotini.client/src/pages/AccountPage.vue
@@ -1,23 +1,43 @@
 <template>
-  <div class="about text-center">
-    <h1>Welcome {{ account.name }}</h1>
-    <h1>Games Played: {{ account.gamesPlayed }}</h1>
-    <img class="rounded" :src="account.picture" alt="" />
-    <p>{{ account.email }}</p>
-    <h1>K-Bucks: {{ account.wallet }}</h1>
-    <h1 role="button" @click="logout">
-      Logout
-    </h1>
-    <button @click="showEditForm = !showEditForm">
-      edit profile
-    </button>
-    <form @submit.prevent="editProfile(account.id)" v-if="showEditForm">
-      <textarea name="name" rows="1" placeholder="name..." v-model="state.newInfo.name"></textarea>
-      <textarea name="picture" rows="1" placeholder="picture..." v-model="state.newInfo.picture"></textarea>
-      <button type="submit">
-        change
-      </button>
-    </form>
+  <div v-if="account.name" class="container">
+    <div class="row text-center my-4">
+      <div class="col-12 about">
+        <h1>WELCOME {{ account.name.toUpperCase() }}</h1>
+      </div>
+    </div>
+    <div class="row my-4">
+      <div class="col-md-3 about text-center">
+        <img class="rounded" :src="account.picture" alt="" />
+        <p class="name">
+          {{ account.name.toUpperCase() }}
+        </p>
+      </div>
+      <div class="col-md-3 d-flex align-items-center">
+        <div class="about text-center">
+          <h3>GAMES PLAYED: {{ account.gamesPlayed }}</h3>
+          <h3>K-BUCKS: {{ account.wallet }}</h3>
+        </div>
+      </div>
+      <div class="col-md-3 ml-auto">
+        <button class="edit-button" @click="showEditForm = !showEditForm">
+          EDIT PROFILE
+        </button>
+        <form @submit.prevent="editProfile(account.id)" v-if="showEditForm">
+          <textarea name="name" rows="1" placeholder="name..." v-model="state.newInfo.name"></textarea>
+          <textarea name="picture" rows="1" placeholder="picture..." v-model="state.newInfo.picture"></textarea>
+          <button class="edit-button" type="submit">
+            UPDATE
+          </button>
+        </form>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-2 ml-auto">
+        <h3 role="button" @click="logout">
+          LOGOUT
+        </h3>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -51,15 +71,33 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.edit-button {
+  font-family: 'Nunito', sans-serif;
+  color: #ff9e00;
+  text-shadow:
+    0 0 .125em hsla(0, 0%, 100%, 0.3),
+    0 0 .45em #ff9e00;
+  font-size: 1.25em;
+  background: none;
+  border: none;
+
+}
+
 .about {
-  h1 {
+  h3, h1 {
   font-family: 'Signika Negative', sans-serif;
   color: #ff9e00;
-
   }
+
+.name {
+  font-family: 'Signika Negative', sans-serif;
+  color: #ff9e00;
+  font-size: 20px;
+}
 }
 
 img {
-  max-width: 100px;
+  height: 200px;
+  max-width: 300px;
 }
 </style>

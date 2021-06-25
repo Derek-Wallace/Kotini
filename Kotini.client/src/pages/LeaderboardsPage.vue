2d34e95f9a266608b8c3e7bc38c0ab4c90e37399
Derek-Wallace <lacrosseid@gmail.com>
2021-06-25 15:16:34 -0600

    leaderboards page

---
+++ Kotini.client/src/pages/LeaderboardsPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-10 mx-auto my-3 text-center">
+        <h1 class="navbar-item">
+          LEADERBOARDS
+        </h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.navbar-item {
+  margin-right: 20px;
+  font-family: 'Nunito', sans-serif;
+  color: #ff9e00;
+  text-shadow:
+    0 0 .125em hsla(0, 0%, 100%, 0.3),
+    0 0 .45em #ff9e00;
+}
+</style>

baf25c2c24e6dafd2bee021d9c37a12eca5c2546
Brian Lipsett <lipsettbrian3@gmail.com>
2021-07-01 11:55:55 -0600

    session id copy

--- Kotini.client/src/pages/SessionPage.vue
+++ Kotini.client/src/pages/SessionPage.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="container-fluid" v-if="session.id">
     <div class="row">
-      <div class="col-lg-12 text-center mt-4 text-break session-title">
-        <h1>Your Session: {{ session.sessionKey }}</h1>
+      <div class="col-lg-12 mt-4 text-break session-title">
+        <span class="session-title d-flex justify-content-center">
+          Your Session: <p class="session-key ml-3" role="button" title="copy session key" @click="copySessionId()">{{ session.sessionKey }}</p>
+          <input type="hidden" id="sessionId" :value="session.sessionKey">
+        </span>
       </div>
       <div class="col-lg-12" v-if="(account.id === session.creatorId)">
         <div class="start-button text-center ml-5 mr-5" @click="createGame(session.sessionKey)">
@@ -61,6 +64,17 @@ export default {
       route: route.params.id,
       async createGame(sid) {
         await gameService.createGame(sid)
+      },
+      copySessionId() {
+        console.log('clikced')
+        const copyText = document.getElementById('sessionId')
+        copyText.setAttribute('type', 'text')
+        copyText.select()
+
+        document.execCommand('copy')
+        copyText.setAttribute('type', 'hidden')
+        window.getSelection().removeAllRanges()
+        Notification.toast('COPIED', 'success')
       }
     }
   }
@@ -120,6 +134,21 @@ export default {
     0 0 1em 0 #ff9e00;
 
   position: relative;
+}
+
+.session-title {
+  font-size: 3rem;
+  font-family: 'Signika Negative', sans-serif;
+  text-shadow:
+    0 0 .125em hsla(0, 0%, 100%, 0.3),
+    0 0 .45em #ff9e00;
+  color: #ff9e00;
+}
+.session-key{
+  transition: all .2s ease;
+}
+.session-key:hover {
+  transform: scale(1.05);
 }
 
 </style>
